cb42b8e172f19ebc964889f93144993e5cc37e47
Benjamin <benjamin142madera@gmail.com>
2023-09-13 22:00:38 -0400

    Agregar archivos de proyecto.

---
+++ Tarea/Empleado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea
+{
+    internal abstract class Empleado: MienbroDeLaComunidad
+
+    {
+
+    }
+}
